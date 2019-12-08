202c320b4a8e8ae1bea2531c6b17f4864a98d41c
Kaykobad Reza <kaykobad.reza@bkash.com>
2019-12-08 16:55:46 +0600

    Add material app to project

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
 import 'opening_pages/splash_page.dart';
 
-void main() => runApp(EyeCaptureSplashScreen());
+void main() => runApp(EyeCapture());
+
+class EyeCapture extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blueGrey,
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: EyeCaptureSplashScreen(),
+    );
+  }
+}
+
