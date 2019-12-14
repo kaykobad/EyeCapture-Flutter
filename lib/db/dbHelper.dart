f94ea42741e6bd039d6409a92547ed22fbb1ccf0
kaykobad <kaykobadreza@gmail.com>
2019-12-14 22:33:51 +0600

    Add db functionality for appointment and images

--- lib/db/dbHelper.dart
+++ lib/db/dbHelper.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:eye_capture/models/appointment_model.dart';
 import 'package:eye_capture/models/patient_model.dart';
+import 'package:eye_capture/models/image_model.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
@@ -56,6 +58,8 @@ class DBProvider {
     );
   }
 
+
+  // patient related functionality
   Future<int> insertPatient(Patient patient) async {
     final db = await database;
     return await db.insert("Patient", patient.toMap());
@@ -70,6 +74,54 @@ class DBProvider {
     final db = await database;
     var res = await db.query("Patient");
     List<Patient> list = res.isNotEmpty ? res.map((c) => Patient.fromMap(c)).toList() : [];
+    return list;
+  }
+
+
+  // appointment related functionality
+  Future<int> insertAppointment(Appointment appointment) async {
+    final db = await database;
+    return await db.insert("Appointment", appointment.toMap());
+  }
+
+  Future<int> deleteAppointmentById(int id) async {
+    final db = await database;
+    return await db.delete("Appointment", where: "id = ?", whereArgs: [id]);
+  }
+
+  Future<int> deleteAppointmentsByPatientId(int patientId) async {
+    final db = await database;
+    return await db.delete("Appointment", where: "patient_id = ?", whereArgs: [patientId]);
+  }
+
+  Future<List<Appointment>> getAllAppointmentsByPatientId(int patientId) async {
+    final db = await database;
+    var res = await db.query("Appointment", where: "patient_id = ?", whereArgs: [patientId]);
+    List<Appointment> list = res.isNotEmpty ? res.map((c) => Appointment.fromMap(c)).toList() : [];
+    return list;
+  }
+
+
+  // image related functionality
+  Future<int> insertImage(Image image) async {
+    final db = await database;
+    return await db.insert("Image", image.toMap());
+  }
+
+  Future<int> deleteImageById(int id) async {
+    final db = await database;
+    return await db.delete("Image", where: "id = ?", whereArgs: [id]);
+  }
+
+  Future<int> deleteImagesByAppointmentId(int appointmentId) async {
+    final db = await database;
+    return await db.delete("Image", where: "appointment_id = ?", whereArgs: [appointmentId]);
+  }
+
+  Future<List<Image>> getAllImagesByAppointmentId(int appointmentId) async {
+    final db = await database;
+    var res = await db.query("Image", where: "appointment_id = ?", whereArgs: [appointmentId]);
+    List<Image> list = res.isNotEmpty ? res.map((c) => Appointment.fromMap(c)).toList() : [];
     return list;
   }
 }
